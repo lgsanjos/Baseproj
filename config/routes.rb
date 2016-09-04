99ec35f037ea02f96a47c12615ac3006322faec0
Luiz Guilherme <lgsanjos@gmail.com>
2016-09-04 05:12:54 -0300

    Moved dashboard content to portfolios' summary

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  get '/portfolios', controller: 'portfolios', action: 'index'
+
   root 'dashboard#index'
 
   # Example of regular route:
